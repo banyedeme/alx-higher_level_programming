b40db8e72a9c2f4006250d128165dcab5a739cc6
banyedeme <banyedeme0@gmail.com>
2023-09-05 17:42:10 +0000

    loops in python

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
-# 6-print_comb3.py
-
-"""Print all possible diff combos of two digits in ascending order.
-
-    The 2 digits must be diff - 01 and 10 are considered identical.
-    """
-for digit1 in range(0, 10):
-    for digit2 in range(digit1 + 1, 10):
-        if digit1 == 8 and digit2 == 9:
-            print("{}{}".format(digit1, digit2))
-        else:
-            print("{}{}".format(digit1, digit2), end=", ")
-
+for number in range(0, 100):
+    if number == 99:
+        print("{}".format(number))
+    else:
+        print("{:02}".format(number), end=", ")
