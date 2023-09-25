66514bdaba1d33533fab3d6c318f8e632f7eaae0
banyedeme <banyedeme0@gmail.com>
2023-09-25 18:00:05 +0000

    handling exceptions in python

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
 
-	try:
-		for count in range(x):
-			print(my_list[count], end=" ")
-		return x
-	except IndexError:
-	  return count
-	 finally:
-	  print()
+    try:
+        for index in range(x):
+            print(my_list[index], end="")
+        return x
+    except IndexError:
+        return index
+    finally:
+        print()
